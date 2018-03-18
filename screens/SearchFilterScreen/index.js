fc8bdd1c891f2f3698052b1fd9f4c5fbbfb8d159
Anzuneet <sdjsk94@gmail.com>
2018-03-18 18:24:19 +0900

    필터 스크린 추가중

--- screens/SearchFilterScreen/index.js
+++ screens/SearchFilterScreen/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {View, Text }from "react-native";
 
-const SearchFilterScreen = props =><Text>Filter~</Text>;
+const SearchFilterScreen = props =><Text>필터</Text>;
 
 export default SearchFilterScreen;
