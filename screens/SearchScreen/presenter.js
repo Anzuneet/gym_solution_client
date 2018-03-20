8202ae560200a966e05b62554a307763dcefdf7a
Anzuneet <sdjsk94@gmail.com>
2018-03-20 16:21:58 +0900

    180320 검색조건

--- screens/SearchScreen/presenter.js
+++ screens/SearchScreen/presenter.js
@@ -40,7 +40,7 @@ const SearchScreens = props => (
         </View>
         <TouchableOpacity style = {styles.filterContainer} onPressOut = {props.searchFilter}>
             <Text style = {styles.filterText}>
-                {props.gyms.lengths}
+                검색 조건 설정하기!
             </Text>
         </TouchableOpacity>
     </View>
